b7a62dbd6220c76cd6ff42a590656a299172cc17
pko22 <pakopm.01@gmail.com>
2025-02-18 14:19:05 +0100

    18/02

--- parte_2/tema_2.2_web/web_ejem1/src/main/java/es/codeurjc/web/GreetingController.java
+++ parte_2/tema_2.2_web/web_ejem1/src/main/java/es/codeurjc/web/GreetingController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class GreetingController {
 
-	@GetMapping("/greeting")
+	@GetMapping("/")
 	public String greeting(Model model) {
 
 		model.addAttribute("name", "World");
--- parte_2/tema_2.2_web/web_ejem2/src/main/java/es/codeurjc/web/Person.java
+++ parte_2/tema_2.2_web/web_ejem2/src/main/java/es/codeurjc/web/Person.java
@@ -10,11 +10,4 @@ public class Person {
         this.surname = surname;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
 }
